2a25d4508214e48630bca40f331d8ca0c63e92f0
ZhuravskyOleh <zhuravskyoleh@gmail.com>
2023-02-21 15:55:59 +0200

    добавил разметку задание 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import * as basicLightbox from 'basiclightbox'
 
-console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryCardItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener('click', onOnGalleryCardClick)
+
+
+
+function createGalleryCardItems(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>`;
+    }).join('');
+}
+
+
+
+function onOnGalleryCardClick(event) {
+    event.preventDefault();
+    console.log(event.target)
+}
+
+// document.querySelector('button.image').onclick = () => {
+
+// 	basicLightbox.create(`
+// 		<img width="1400" height="900" src="https://placehold.it/1400x900">
+// 	`).show()
+
+// }
