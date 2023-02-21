8cc72ab5c38a4392733d71cfafe05e7180fd4502
ZhuravskyOleh <zhuravskyoleh@gmail.com>
2023-02-21 22:32:22 +0200

    второе задание без description

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryCardItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryContainer.addEventListener('click', onGalleryCadrClick);
+
+function createGalleryCardItems(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </div>`;
+    }).join('');
+}
+
+function onGalleryCadrClick(event) {
+    event.preventDefault();
+    console.log(event.target.dataset);
+    var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+}
+
